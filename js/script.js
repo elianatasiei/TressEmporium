b8eb372b34ddbcf132313266f66c8bb4a87b9f2f
Eliana Atasiei <elianatasiei@gmail.com>
2023-03-21 11:00:38 +0000

    added navbar click event and glassmorphism style

--- js/script.js
+++ js/script.js
@@ -1,4 +1,42 @@
 /* Implementation of Javascript for a smooth scrolling when links / buttons are clicked, moving to respective section */
+"use scrict";
+
+/* lets content of page load before navbar event */
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.querySelector(".navbar");
+  var scrollFlag = false;
+
+  navbar.addEventListener("click", function () {
+    if (scrollFlag) {
+      scrollToTop();
+      scrollFlag = false;
+    } else {
+      scrollFlag = true;
+      setTimeout(function () {
+        scrollFlag = false;
+      }, 300);
+    }
+  });
+
+  var lastClickTime = 0;
+
+  navbar.addEventListener("click", function () {
+    var now = new Date().getTime();
+    if (now - lastClickTime < 300) {
+      scrollToTop();
+      lastClickTime = 0;
+    } else {
+      lastClickTime = now;
+    }
+  });
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+});
 
 const servicesLink = document.querySelector('a[href="#services"]');
 const contactLink = document.querySelector('a[href="#contact"]');
